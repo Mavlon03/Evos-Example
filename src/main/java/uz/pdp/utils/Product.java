aa81143074637225884a12f395d4c5a133310143
Akmalov Mavlon <akmalovmavlon5@gmail.com>
2024-10-01 14:03:10 +0500

    Master

---
+++ src/main/java/uz/pdp/utils/Product.java
@@ -0,0 +1,17 @@
+package uz.pdp.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private Long product_Id;
+    private String name;
+    private Integer price;
+    private String imagePath;
+    private Long category_Id;
+}
